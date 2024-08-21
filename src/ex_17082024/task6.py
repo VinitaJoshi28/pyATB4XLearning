4abe85347ab9ec53ad325c8f84a37c6115d871ea
Vinita <vinijoshi012@gmail.com>
2024-08-21 15:13:43 -0700

    updated Homework Task6; 17 Aug 2024

---
+++ src/ex_17082024/task6.py
@@ -0,0 +1,10 @@
+#Develop a Python script that calculates the square and cube of a given number.
+import math
+
+number=int(input("Enter a number\n"))
+cube=math.pow(number,3)
+cube1=number*number*number
+cube2=pow(number,3)
+print("The cube of the number is : ",cube)
+print("The cube of the number is : ", cube1)
+print("The cube of the number is : ", cube2)
